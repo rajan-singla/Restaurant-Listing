f41b7ba1017ad9f3057b27489700797f2f5b4b18
RAJAN-PC\lenovo <rajanabohar@gmail.com>
2021-05-08 20:55:09 +0530

    #New  - Added media queries  - Added enums for checking the orientation type  - Added Device_Preview plugin in pubspec.yaml

--- restaurant_listings/lib/ui_screens/restaurant_containers.dart
+++ restaurant_listings/lib/ui_screens/restaurant_containers.dart
@@ -1,63 +1,50 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:restaurant_listings/MediaQueryClasses/different_sizes.dart';
+import 'package:restaurant_listings/MediaQueryClasses/size_config.dart';
 import 'package:restaurant_listings/Models/home_model.dart';
 import 'package:restaurant_listings/constants/constants.dart';
+import 'package:restaurant_listings/enums/device_screen_type.dart';
 
 class RestaurantContainers extends StatefulWidget {
-  RestaurantContainers({Key key, this.nearByData}) : super(key: key);
+  RestaurantContainers({Key key, this.nearByData, this.orientationType}) : super(key: key);
   final List<NearBy> nearByData;
+  final DeviceScreenType orientationType;
 
   @override
   _RestaurantContainersState createState() => _RestaurantContainersState();
 }
 
 class _RestaurantContainersState extends State<RestaurantContainers> {
-  final List<Widget> carouselImages = [
-    Image.asset("assets/pic1.jpg", fit: BoxFit.cover),
-    Image.asset(
-      "assets/pic2.jpg",
-      fit: BoxFit.cover,
-    ),
-    Image.asset(
-      "assets/pic3.jpg",
-      fit: BoxFit.cover,
-    ),
-    Image.asset(
-      "assets/pic4.jpg",
-      fit: BoxFit.cover,
-    ),
-    Image.asset(
-      "assets/pic5.jpg",
-      fit: BoxFit.cover,
-    ),
-  ];
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
+
 
     List<NearBy> nearbyList = widget.nearByData;
 
     return SizedBox(
-      height: 220.0,
-      width: size.width,
+      height: size220(widget.orientationType),
+      width: SizeConfig.screenFullWidth,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
         itemCount: nearbyList.length,
         itemBuilder: (context, index) {
           return Padding(
             padding: index == nearbyList.length - 1
-                ? EdgeInsets.fromLTRB(16.0, 0, 16.0, 0)
-                : EdgeInsets.fromLTRB(16.0, 0, 0, 0),
-            child: IntrinsicWidth(
+                ? EdgeInsets.fromLTRB(size16(widget.orientationType), 0, size16(widget.orientationType), 0)
+                : EdgeInsets.fromLTRB(size16(widget.orientationType), 0, 0, 0),
+            child: SizedBox(
+              width: SizeConfig.screenFullWidth * 0.65,
               child: Column(
                 mainAxisSize: MainAxisSize.max,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    width: size.width * 0.65,
-                    height: 120.0,
+                    width: SizeConfig.screenFullWidth * 0.65,
+                    height: size120(widget.orientationType),
                     child: ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
+                      borderRadius: BorderRadius.circular(size8(widget.orientationType)),
                       child: Image.network(
                         ConstantUrl.URL + nearbyList[index].images[0].image,
                         fit: BoxFit.cover,
@@ -65,60 +52,61 @@ class _RestaurantContainersState extends State<RestaurantContainers> {
                     ),
                   ),
                   SizedBox(
-                    height: 8.0,
+                    height: size8(widget.orientationType),
                   ),
-                  Flexible(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        child: Text(
                           nearbyList[index].name,
+                          overflow: TextOverflow.ellipsis,
                           style: TextStyle(
-                              fontSize: 16.0, fontWeight: FontWeight.bold),
+                              fontSize: size16(widget.orientationType), fontWeight: FontWeight.bold),
                         ),
-                        Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                                width: 2.0, color: Colors.lightGreen[700]),
-                            color: Colors.lightGreen[700],
-                            borderRadius: BorderRadius.circular(50.0),
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              width: size2(widget.orientationType), color: Colors.lightGreen[700]),
+                          color: Colors.lightGreen[700],
+                          borderRadius: BorderRadius.circular(size50(widget.orientationType)),
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(
+                              horizontal: size8(widget.orientationType), vertical: size2(widget.orientationType)),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.star,
+                                color: Colors.white,
+                                size: size15(widget.orientationType),
+                              ),
+                              SizedBox(
+                                width: size5(widget.orientationType),
+                              ),
+                              Text(
+                                nearbyList[index]
+                                            .ratingAvg
+                                            .toString()
+                                            .length ==
+                                        1
+                                    ? '${nearbyList[index].ratingAvg}.0'
+                                    : '${nearbyList[index].ratingAvg}',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
                           ),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 8.0, vertical: 2.0),
-                            child: Row(
-                              children: [
-                                Icon(
-                                  Icons.star,
-                                  color: Colors.white,
-                                  size: 15.0,
-                                ),
-                                SizedBox(
-                                  width: 5.0,
-                                ),
-                                Text(
-                                  nearbyList[index]
-                                              .ratingAvg
-                                              .toString()
-                                              .length ==
-                                          1
-                                      ? '${nearbyList[index].ratingAvg}.0'
-                                      : '${nearbyList[index].ratingAvg}',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
+                        ),
+                      )
+                    ],
                   ),
                   Text(
                     nearbyList[index].category.name,
                     style: TextStyle(color: Colors.grey),
                   ),
                   SizedBox(
-                    height: 8.0,
+                    height: size8(widget.orientationType),
                   ),
                   Text(
                     r'Avg Cost - $' +
@@ -127,7 +115,7 @@ class _RestaurantContainersState extends State<RestaurantContainers> {
                     style: TextStyle(color: Colors.black87),
                   ),
                   SizedBox(
-                    height: 8.0,
+                    height: size8(widget.orientationType),
                   ),
                   Flexible(
                     child: Row(
@@ -136,16 +124,16 @@ class _RestaurantContainersState extends State<RestaurantContainers> {
                         Icon(
                           Icons.location_on,
                           color: Colors.grey,
-                          size: 15.0,
+                          size: size15(widget.orientationType),
                         ),
                         SizedBox(
-                          width: 5.0,
+                          width: size5(widget.orientationType),
                         ),
                         Flexible(
                           child: Text(
                             nearbyList[index].address,
                             style:
-                                TextStyle(fontSize: 12.0, color: Colors.grey),
+                                TextStyle(fontSize: size12(widget.orientationType), color: Colors.grey),
                           ),
                         ),
                       ],
@@ -161,24 +149,24 @@ class _RestaurantContainersState extends State<RestaurantContainers> {
   }
 }
 
-class AllHomeImages {
-  static List<Widget> carouselImages = [
-    Image.asset("assets/pic1.jpg", fit: BoxFit.cover),
-    Image.asset(
-      "assets/pic2.jpg",
-      fit: BoxFit.cover,
-    ),
-    Image.asset(
-      "assets/pic3.jpg",
-      fit: BoxFit.cover,
-    ),
-    Image.asset(
-      "assets/pic4.jpg",
-      fit: BoxFit.cover,
-    ),
-    Image.asset(
-      "assets/pic5.jpg",
-      fit: BoxFit.cover,
-    ),
-  ];
-}
+// class AllHomeImages {
+//   static List<Widget> carouselImages = [
+//     Image.asset("assets/pic1.jpg", fit: BoxFit.cover),
+//     Image.asset(
+//       "assets/pic2.jpg",
+//       fit: BoxFit.cover,
+//     ),
+//     Image.asset(
+//       "assets/pic3.jpg",
+//       fit: BoxFit.cover,
+//     ),
+//     Image.asset(
+//       "assets/pic4.jpg",
+//       fit: BoxFit.cover,
+//     ),
+//     Image.asset(
+//       "assets/pic5.jpg",
+//       fit: BoxFit.cover,
+//     ),
+//   ];
+// }
